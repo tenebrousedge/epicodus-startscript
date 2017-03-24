d085d151b41b0f0e0ec0f6fcd776daa3b31038d6
Patrick Leahy <tenebrousedge@gmail.com>
2017-03-24 04:06:27 -0700

    adds a README and a lot of improvements to the script. It may even work!
    We quoted all the variable names, test if paths exist (and whether ~/bin exists in $PATH)
    I'm not going to be entirely sure that this works until Monday at the earliest
    The new project script needs to be updated to run the pre-commit installer.

---
+++ README.md
@@ -0,0 +1,25 @@
+# Epic Start
+
+This is a shell script which is intended to set up a new workstation with some useful stuff. It's intended as a start-of-day process for Epicodus, but may be more generally useful (who knows). As of the time of this writing, I haven't actually tested this.
+
+## Usage
+
+So generally you just need to drop this somewhere and run it.
+
+* clone the repo: `git clone https://github.com/tenebrousedge/epicodus-startscript ~/startscript`
+* run the script by typing `~/startscript/startup_script.sh`
+* get coffee, this may take a few minutes
+* type `zsh`
+
+Check out `prompt -p` if you want to see what prompts are available, and (example) `prompt steeef` to set the one you want.
+You also get the new project creation script, so you can type `new_project` from anywhere, type in the project name, and it will create a new project in a folder with that name in your desktop.
+
+Happy coding!
+
+## Dependencies
+
+This script requires npm, Atom, git, and zsh to be installed or it will fail horribly. All of these things should be installed by default at Epicodus.
+
+## License
+Copyright 2017 by Kai
+licensed CC0
--- startup_script.sh
+++ startup_script.sh
@@ -1,43 +1,66 @@
-#!/bin/bash
+#!/bin/bash -u
 
 # clone prezto
 
-git clone --recursive https://github.com/tenebrousedge/prezto $HOME/.zprezto
+git clone --recursive 'https://github.com/tenebrousedge/prezto' "$HOME"/.zprezto
 
 # the way prezto does this is much more elegant. This should be more robust.
 ZSH_DOTFILES=(zshrc zshenv zshlogin zshlogout zprofile zpreztorc)
-for file in $ZSH_DOTFILES; do echo "$HOME/.zprezto/runcoms/$file" "$HOME/.$file"; done
+for dotfile in "$ZSH_DOTFILES"; do echo "$HOME/.zprezto/runcoms/$dotfile" "$HOME/.$dotfile"; done
 
 # clone the repo creator
 exec_dir=$HOME/bin
-mkdir $exec_dir
-git clone https://github.com/tenebrousedge/new_project_script $exec_dir 
+mkdir "$exec_dir"
+git clone 'https://github.com/tenebrousedge/new_project_script' "$exec_dir" 
 
 # set up nano
 
-git clone https://github.com/scopatz/nanorc $HOME/.nano
-cat $HOME/.nano/nanorc >> $HOME/.nanorc
+git clone 'https://github.com/scopatz/nanorc' "$HOME"/.nano
+cat "$HOME"/.nano/nanorc >> "$HOME"/.nanorc
 
 # git config
 
-git config --global core.editor nano
-git config --global core.autocrlf input
-git config --global alias.ignore  'update-index --assume-unchanged'
-git config --global alias.unignore 'update-index --no-assume-unchanged'
-git config --global alias.ignored '!git ls-files -v | grep '^h'
+git config --global 'core.editor' $(which nano)
+git config --global 'core.autocrlf' 'input'
+git config --global 'alias.ignore'  'update-index --assume-unchanged'
+git config --global 'alias.unignore' 'update-index --no-assume-unchanged'
+git config --global 'alias.ignored' '!git ls-files -v | grep "^h\"'
 
 # git hooks
 
-https://github.com/pre-commit/pre-commit.git
+curl http://pre-commit.com/install-local.py | python
+
+cat >>"$HOME"/.pre-commit-config.yaml <<'EOM'
+-   repo: git://github.com/pre-commit/pre-commit-hooks
+    sha: v0.4.2
+    hooks:
+    -   id: trailing-whitespace
+    -   id: check-byte-order-marker
+-   repo: git://github.com/pre-commit/mirrors-eslint
+    sha: 3.14.0
+    hooks:
+    -   id: eslint
+EOM
+
+# npm
+
+npm install 'eslint'
 
 # atom config
 
-apm install file-icons
-apm install linter-csslint
-apm install minimap
+apm install 'file-icons'
+apm install 'linter-csslint'
+apm install 'linter-eslint'
+apm install 'minimap'
+apm install 'minimap-hide'
+apm install 'highlight-selected'
+apm install 'minimap-highlight-selected'
+apm install 'autoclose-html'
+apm install 'pigments'
 
 # environment
 
-export CDPATH=$HOME/Desktop
+export CDPATH="$HOME"/Desktop
 ## n.b. PATH is normally set in one's bashrc or zshrc. This script sets it here for reasons.
-export PATH=$PATH:$exec_dir
+export PATH="$PATH:$exec_dir"
+exit 0
