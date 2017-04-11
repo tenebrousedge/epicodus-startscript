14a73ac1795b1b346cfc6690d0de0a1a8d2acc70
Patrick Leahy <tenebrousedge@gmail.com>
2017-04-11 08:07:07 -0700

    use XDG_DATA_HOME

--- startup_script.sh
+++ startup_script.sh
@@ -2,28 +2,37 @@
 
 # clone prezto
 
-git clone --recursive 'https://github.com/tenebrousedge/prezto' "$HOME"/.zprezto
+echo "Installing Prezto"
+git clone --recursive 'https://github.com/tenebrousedge/prezto' "$HOME"/.zprezto > /dev/null
 
 # the way prezto does this is much more elegant. This should be more robust.
 declare -a ZSH_DOTFILES=('zshrc' 'zshenv' 'zshlogin' 'zshlogout' 'zprofile' 'zpreztorc')
 for dotfile in "${ZSH_DOTFILES[@]}"; do ln -s "$HOME/.zprezto/runcoms/$dotfile" "$HOME/.$dotfile"; done
 
 # Make zsh the login shell
+# Using $HOME/.profile didn't seem to work?
+echo '[ -f /bin/zsh ] && exec /bin/zsh -l' >> "$HOME/.bashrc"
 
-echo '[ -f /bin/zsh ] && exec /bin/zsh -l' >> "$HOME/.profile"
-
-# clone the repo creator
+# Specification:
+# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+data_home=${XDG_DATA_HOME:-"$HOME/.local/share"}
+# nonstandard except on Ubuntu/Mint but yanno
 exec_dir="$HOME/.local/bin"
-if [[ ! -d "$exec_dir" ]]; then
-mkdir -p "$exec_dir"
-fi
 
-np_dir="$HOME/new_project_script"
+echo 'Creating Directories'
+if [[ ! -d "$data_home" ]]; then
+  mkdir -p "$data_home"
+fi
+if [[ ! -d "$exec_dir" ]]; then
+  mkdir -p "$exec_dir"
+fi
+# clone the repo creator
+np_dir="$data_home/new_project_script"
 git clone 'https://github.com/tenebrousedge/new_project_script' "$np_dir"
 ln -s "$np_dir/repo_init.sh" "$exec_dir/new_project" 
 # set up nano
 
-(brew install nano >/dev/null 2>&1)
+brew install nano >/dev/null
 git clone 'https://github.com/scopatz/nanorc' "$HOME"/.nano
 mv "$HOME/.nano/nanorc" "$HOME/.nanorc"
 
@@ -37,7 +46,7 @@ git config --global 'alias.ignored' '!git ls-files -v | grep "^h\"'
 
 # git hooks
 
-curl 'http://pre-commit.com/install-local.py' | python
+curl 'http://pre-commit.com/install-local.py' | python >/dev/null
 
 cat >>"$HOME"/.pre-commit-config.yaml <<'EOM'
 -   repo: git://github.com/pre-commit/pre-commit-hooks
